0a19ac985bc92dde4d3594c975002ad443a04443
Qurgs <114207624+Qurgs@users.noreply.github.com>
2022-11-17 23:59:26 +0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+# Слито в https://t.me/HACKER_PHONE_VIP
+
+Admin = "https://t.me/HACKER_PHONE_VIP" # Ссылка на админа
+token = "5204611885:AAHK8CnhEAcJM7oCYIqqclMiMToAkMAE8ss" # Токен бота с @BotFather
+
+qiwi = "Киви" # Ваш QIWI
+yandex_money = "Яндекс деньги" # Ваш ЮMoney
+payeer = "Пеер" # Ваш PAYEER
+
+# Слито в https://t.me/HACKER_PHONE_VIP
