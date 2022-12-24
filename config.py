d6fb5c71c0cfcf311879d2b360dfef499f573bfc
Qurgs <114207624+Qurgs@users.noreply.github.com>
2022-12-24 19:40:56 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,8 @@
-# Слито в https://t.me/HACKER_PHONE_VIP
 
-Admin = "https://t.me/HACKER_PHONE_VIP" # Ссылка на админа
-token = "5204611885:AAHK8CnhEAcJM7oCYIqqclMiMToAkMAE8ss" # Токен бота с @BotFather
+
+Admin = "https://t.me/lspwncvv" 
+token = "5204611885:AAHK8CnhEAcJM7oCYIqqclMiMToAkMAE8ss"
 
 qiwi = "Киви" # Ваш QIWI
 yandex_money = "Яндекс деньги" # Ваш ЮMoney
 payeer = "Пеер" # Ваш PAYEER
-
-# Слито в https://t.me/HACKER_PHONE_VIP
